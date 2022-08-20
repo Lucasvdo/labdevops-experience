f7bdbf73b3635a2f913670a916566043c9a5c932
Lucas Valerio de Oliveira <lucasvalerio@live.com>
2022-08-20 09:27:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,16 +17,5 @@ def pagina_inicial():
 </html>
 """
 
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
-
 if __name__ == '__main__':
     app.run()
